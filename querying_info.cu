ba88ac173737feb189945b4ca340161c5049e152
ashwins9001 <nitromekid@hotmail.com>
2020-02-26 23:15:24 +0530

    More querying commands

--- querying_info.cu
+++ querying_info.cu
@@ -17,4 +17,20 @@ int main(void) {
 		cout << "Total global memory: " << devProp.totalGlobalMem << endl;
 		printf("Max thread dimensions: (%d, %d, %d)\n", devProp.maxThreadsDim[0], devProp.maxThreadsDim[1], devProp.maxThreadsDim[2]);
 	}
+
+	//Check for GPUs capable of double-precision floating-point math, only available on those w computer capability >= 1.3
+	//Fill block of mem w particular struct that is threshold, then use cudaChooseDevice to find one with best criteria
+	//Lower overhead, instead of iterating through all in for loop
+	int deviceID;
+	cudaGetDevice(&deviceID);
+
+	//copy obj for first 0 iterations and allocate mem 
+	memset(&devProp, 0, sizeof(cudaDeviceProp));
+	devProp.major = 1;
+	devProp.minor = 3;
+	cudaChooseDevice(&deviceID, &devProp);
+	cout << "Device ID with closest capability: " << deviceID << endl;
+
+	//host comms w this device now
+	cudaSetDevice(deviceID);
 }
