0f9af6b3434a76ed95cefd525edbcd418cd4a366
ashwins9001 <nitromekid@hotmail.com>
2020-03-03 13:11:39 +0530

    Updated w device kernel

--- gpu_ripple.cu
+++ gpu_ripple.cu
@@ -3,7 +3,8 @@
 #include "cpu_anim.h"
 using namespace std;
 #include <device_launch_parameters.h>
-#define DIM 1000
+//Window dim
+#define DIM 512 
 
 //Create struct to hold device and host pointers to image bitmap
 struct DataBlock {
@@ -27,6 +28,26 @@ void generate_frame(DataBlock *d, int render)
 }
 
 	
+__global__ void kernel(unsigned char *ptr, int ticks)
+{
+	//Index one of the threads to an image pos
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+
+	float fx = x - DIM / 2;
+	float fy = y - DIM / 2;
+	float d = sqrtf(fx * fx + fy * fy);
+	//Create varying grey vals depending on pixel 
+	unsigned char grey = (unsigned char)(128.0f + 127.0f * cos(d / 10.0f - ticks / 7.0f) / (d / 10.0f + 1.0f));
+
+	//Offset into output buffer for window generation when ready 
+	ptr[offset * 4 + 0] = grey;
+	ptr[offset * 4 + 1] = grey;
+	ptr[offset * 4 + 2] = grey;
+	ptr[offset * 4 + 3] = 255;
+}
+
 int main(void)
 {
 	DataBlock data;
