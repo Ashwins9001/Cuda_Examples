4e0d5ab1c4e0264030efa258e1c16f1f06dccd25
ashwins9001 <nitromekid@hotmail.com>
2020-03-12 17:11:45 +0530

    Added thread sync and cache reduction

--- dot_product.cu
+++ dot_product.cu
@@ -1,8 +1,14 @@
 #include <cuda_runtime.h>
 #include<iostream>
-#include "cpu_bitmap.h"
 using namespace std;
 #include <device_launch_parameters.h>
+#ifndef __CUDACC__ 
+#define __CUDACC__
+#endif
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+
+
 
 /*
 CUDA C has a __shared__ memory section where a copy of var is made for each block & threads within a block can all access var but cannot see or modify copy in other blocks
@@ -30,4 +36,24 @@ __global__ void dot(float *a, float *b, float *c)
 		threadID += blockDim.x * gridDim.x;
 	}
 	cache[cacheID] = temp; //shared mem buffer to store running sum per thread 
+	//Apply reduction to sum vals, whereby input arr made into smaller output arr
+	//Apply multiple threads for sum, each one adds two vals of cache[], resulting in log2(threadsPerBlock) steps 
+	//Each thread does two computations, therefore 2x per thread. There are threadsPerBlock running in parallel, thus 2^threadsPerBlock computations being done per step
+	int i = blockDim.x / 2; //each thread does two tasks, thus need half as many
+	while (i != 0) //run in parallel so each step halves size of cache until reach 1 elem in arr
+	{
+		if (cacheID < i) //check cacheID being summed less than num operators
+		{
+			cache[cacheID] += cache[cacheID + i]; //add curr cache val to ith 
+		}
+		__syncthreads();
+		i /= 2; //every other cache index
+	}
+	//Final reduction, each block has single sum left & store to global mem
+	if (cacheID == 0)
+	{
+		c[blockIdx.x] = cache[0]; //send to curr block 
+	}
 }
+
+int main(void) {}
